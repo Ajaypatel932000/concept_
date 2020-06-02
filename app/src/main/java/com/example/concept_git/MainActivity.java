65f007a3e96d33f3e143c98a09baa819ecd3f4c7
AJAY <ap210484@gmail.com>
2020-06-02 17:03:39 +0530

    Splash Screen

--- app/src/main/java/com/example/concept_git/MainActivity.java
+++ app/src/main/java/com/example/concept_git/MainActivity.java
@@ -2,16 +2,50 @@ package com.example.concept_git;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private  static  int SPLASH_SCREEN =6000;
+
+    ImageView imageView;
+    TextView textView1, textView2;
+    Animation top, bottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+      getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this,"Hello world",Toast.LENGTH_LONG).show();
+
+        imageView = findViewById(R.id.imageView);
+         textView1 = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+
+
+        top = AnimationUtils.loadAnimation(this, R.anim.top);
+        bottom = AnimationUtils.loadAnimation(this, R.anim.buttom);
+        imageView.setAnimation(top);
+        textView1.setAnimation(bottom);
+        textView2.setAnimation(bottom);
+
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, login.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
     }
 }
